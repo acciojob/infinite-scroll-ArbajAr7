ca45e04734861780c0e23048547a4b83aedeb947
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-21 13:58:45 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here!
-
+var count = 1;
+for(let i = 0; i<10; i++) 
+	{
+		document.getElementById('infi-list').innerHTML += `<li>Item ${count}</li>`;
+		count += 1;
+	}
+document.getElementById('infi-list').addEventListener("scrollend", (event) => {
+  for(let i =0; i<2; i++)
+	  {
+		  document.getElementById('infi-list').innerHTML += `<li>Item ${count}</li>`;
+		  count +=1;
+	  }
+});
